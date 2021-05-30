23d357b8fe5a86a9e41245e4e4dcc652cab8afb2
AeeUX <AndreasEriksen99@hotmail.com>
2021-05-31 01:24:29 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 // if IOS -> UIView
 // if Android -> AndroidView
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1}>Hello React Native!</Text>
+      <Image source={{ 
+        width: 200,
+        height: 200,
+        uri:"https://images.unsplash.com/photo-1530018352490-c6eef07fd7e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=661&q=80" }} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "whitesmoke",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
