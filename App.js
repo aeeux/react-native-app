c8793c21f2ad2a4df21d9f7b68b90083a98f5a1d
AeeUX <AndreasEriksen99@hotmail.com>
2021-05-30 23:59:53 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// if IOS -> UIView
+// if Android -> AndroidView
 export default function App() {
   return (
     <View style={styles.container}>
