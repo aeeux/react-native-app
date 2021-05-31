f9f0f7d28e2c5fd63dbfaa901b18871954cd58a2
AeeUX <AndreasEriksen99@hotmail.com>
2021-05-31 04:13:31 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
-import WelcomeScreen from './app/assets/screens/WelcomeScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 // if IOS -> UIView
 // if Android -> AndroidView
