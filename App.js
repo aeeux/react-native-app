73f1a752d909b8c5d4382b45a70b6b0290187e15
AeeUX <AndreasEriksen99@hotmail.com>
2021-05-31 04:49:45 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import React from 'react';
 import { View } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 // if IOS -> UIView
 // if Android -> AndroidView
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
