a03d902986f2d336b154c9bda34e41e3e4717757
AeeUX <AndreasEriksen99@hotmail.com>
2021-05-31 03:02:38 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,29 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Dimensions, StyleSheet, SafeAreaView, Statusbar, Platform, View } from 'react-native';
+import { View } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 // if IOS -> UIView
 // if Android -> AndroidView
 export default function App() {
-  const {landscape} = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
       <View style={{ 
-        backgroundColor: 'dodgerblue',
-        width: '100%',
-        height: landscape ? "100%" : "30%",
-        }}></View>
-    </SafeAreaView>
+        backgroundColor: 'whitesmoke',
+        flex: 1,
+        flexDirection: "row", //horizontal
+        justifyContent: "center", //main-axis
+        alignItems: "center", //secondary-axis
+        flexWrap: "wrap"
+      }}
+      >
+        <View style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100
+        }}
+        />
+          <View style={{
+            backgroundColor: "gold",
+            width: 100,
+            height: 100
+          }}
+          />
+          <View style={{
+            backgroundColor: "tomato",
+            width: 100,
+            height: 100
+          }}
+          />
+          <View style={{
+            backgroundColor: "grey",
+            width: 100,
+            height: 100
+          }}
+          />
+          <View style={{
+            backgroundColor: "greenyellow",
+            width: 100,
+            height: 100
+          }}
+          />
+      </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "whitesmoke",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
