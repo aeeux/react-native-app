4702e626c41a179ee4c07b8269eb95158556eded
AeeUX <AndreasEriksen99@hotmail.com>
2021-05-31 02:35:38 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Dimensions, StyleSheet, SafeAreaView, Statusbar, Platform, View } from 'react-native';
-
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 // if IOS -> UIView
 // if Android -> AndroidView
 export default function App() {
+  const {landscape} = useDeviceOrientation();
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ 
         backgroundColor: 'dodgerblue',
         width: '100%',
-        height: '30%',
+        height: landscape ? "100%" : "30%",
         }}></View>
     </SafeAreaView>
   );
